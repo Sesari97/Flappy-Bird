52e86c0819d713b0f8ccef09946f408dbec5cb31
Sesari97 <sesari97@gmail.com>
2022-11-29 13:41:16 +0100

    Add obstacle generator
    Creation of the obstacle generator script

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    public float timeMax = 1;
+    private float timeInit = 0;
+    public GameObject obstacle;
+    public GameObject obstacleOrigin;
+    public float high = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obstacleNew = Instantiate(obstacle);
+        obstacleNew.transform.position = obstacleOrigin.transform.position;
+        Destroy(obstacleNew, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeInit> timeMax)
+        {
+            GameObject obstacleNew = Instantiate(obstacle);
+            obstacleNew.transform.position = obstacleOrigin.transform.position + new Vector3(0, Random.Range(-high, high), 0);
+            Debug.Log(obstacleNew.transform.position);
+            Debug.Log(Random.Range(-high, high));
+            Destroy(obstacleNew, 5);
+            timeInit = 0;
+        }
+        else
+        {
+            timeInit += Time.deltaTime;
+        }
+    }
+}
